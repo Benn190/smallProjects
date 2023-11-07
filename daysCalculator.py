a3b1dce975e719ff4058463be5af238001e8c3ef
Benn190 <benahine@gmail.com>
2023-11-07 23:57:44 +0000

    Implemented isLeapYear method

--- daysCalculator.py
+++ daysCalculator.py
@@ -33,3 +33,20 @@ month2 = int(month2)
 
 daysUntil = 0
 
+def isLeapYear(year):
+    if year % 100 == 0:
+        if year % 400 == 0:
+            return True
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+        
+
+    return true
+
+def daysLeftInMonth(year, month, days):
+    if month == 1:
+        daysUntil = 31 - days
+    elif month == 2 and year:
+        daysUntil = 31 - days
