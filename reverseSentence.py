8e22f33886dc4001b323041c7ad89c2f9230f05a
Benn190 <benahine@gmail.com>
2023-11-07 23:14:10 +0000

    additional comments

--- reverseSentence.py
+++ reverseSentence.py
@@ -7,17 +7,18 @@ currWord = "" # hold current word
 reversedWords = [] #contains a list of the reversed words collected
 
 for l in range(0, len(userInput)):
+    # space means new word, so add current word to list then delete it
     if userInput[l] == " ":
         reversedWords.append(currWord)
         currWord = ""
+    # case for last character in string
     elif l == len(userInput)-1:
         currWord = userInput[l] + currWord
         reversedWords.append(currWord)
+    # case for consecutive characters to be added onto existing current word
     else:
         currWord = userInput[l] + currWord
 
 #print from reversed list
 for word in reversedWords:
     print(word, end=" ")
-        
-        
